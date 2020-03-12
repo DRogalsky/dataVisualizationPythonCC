ee545d5bba868e86e4f21281e449dd303bd8daa2
dnl092 <drogalsky14@my.whitworth.edu>
2020-03-12 10:00:10 -0700

    made the random walk visualization repeating and clearer

--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,26 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#make a random walk
-rw = RandomWalk()
-rw.fill_walk()
+#keep making new walks as long as the program is active
+while True:
+    #make a random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-#plot the points in the walk
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    #plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.copper,
+     edgeColors = 'none' ,s=15)
+
+    # Emphasize the first and last points
+    ax.scatter(0, 0, c='green', edgecolors = 'none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1],c='red', edgecolors='none',
+    s=100)
+
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
